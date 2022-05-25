9b0ff12d03fc7a64b2d6c584b3b75c2078297d48
Simeon <simeonberns@gmail.com>
2022-05-25 14:57:51 -0500

    Added heroku app to origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200'
+    origins 'http://localhost:4200', 'https://item-sale-dashboard.herokuapp.com'
 
     resource '*',
       headers: :any,
